2a39195161240406c357db55f5f6421a3d6ece37
ziontee113 <ziontee113@gmail.com>
2023-04-26 16:35:33 +0700

    draft(ui): rewrite class replacing function

--- lua/Gambit/ui/replace_classes_menu.lua
+++ lua/Gambit/ui/replace_classes_menu.lua
@@ -11,6 +11,62 @@ local classes_groups = {
     { "flex", "flex-row" },
 }
 
+local myfunc = function(winnr, bufnr, item)
+    local desired_types = { "jsx_element", "jsx_self_closing_element" }
+    local jsx_node = lib_ts.find_parent(winnr, desired_types)
+
+    local root
+    if jsx_node:type() == "jsx_element" then
+        local opening_element =
+            lib_ts.get_children_that_matches_types(jsx_node, { "jsx_opening_element" })[1]
+        root = opening_element
+    elseif jsx_node:type() == "jsx_self_closing_element" then
+        root = jsx_node
+    end
+
+    local _, matched_groups = lib_ts.get_all_nodes_matches_query(
+        [[ ;query
+(jsx_attribute
+  (property_identifier) @prop_ident (#eq? @prop_ident "className")
+  (string) @string
+)
+]],
+        "tsx",
+        root,
+        { "string" }
+    )
+
+    local className_string_node = matched_groups["string"][1]
+
+    local old_classes = ""
+    if className_string_node then
+        old_classes = vim.treesitter.get_node_text(className_string_node, bufnr)
+        old_classes = string.gsub(old_classes, "[\"']", "")
+    end
+
+    local new_classes =
+        manipulator.replace_classes_with_list_item(old_classes, classes_groups, item.data)
+    new_classes = string.format('"%s"', new_classes)
+
+    local jsx_tag_node = lib_ts.get_children_that_matches_types(root, { "identifier" })[1]
+
+    if jsx_tag_node then
+        if className_string_node then
+            lib_ts.replace_node_text(bufnr, className_string_node, new_classes)
+        else
+            local _, _, end_row, end_col = jsx_tag_node:range()
+            vim.api.nvim_buf_set_text(
+                bufnr,
+                end_row,
+                end_col,
+                end_row,
+                end_col,
+                { string.format(" className=%s", new_classes) }
+            )
+        end
+    end
+end
+
 M.show_menu = function()
     local old_winnr, old_bufnr = vim.api.nvim_get_current_win(), vim.api.nvim_get_current_buf()
 
@@ -30,34 +86,7 @@ M.show_menu = function()
             submit = { "<CR>", "<Space>", "l" },
         },
         on_submit = function(item)
-            local tag_node, className_prop_identifier_node, className_string_node =
-                lib_ts.get_className_related_nodes(old_winnr)
-
-            local old_classes = ""
-            if className_string_node then
-                old_classes = vim.treesitter.get_node_text(className_string_node, old_bufnr)
-                old_classes = string.gsub(old_classes, "[\"']", "")
-            end
-
-            local new_classes =
-                manipulator.replace_classes_with_list_item(old_classes, classes_groups, item.data)
-            new_classes = string.format('"%s"', new_classes)
-
-            if tag_node then
-                if className_prop_identifier_node then
-                    lib_ts.replace_node_text(old_bufnr, className_string_node, new_classes)
-                else
-                    local _, _, end_row, end_col = tag_node:range()
-                    vim.api.nvim_buf_set_text(
-                        old_bufnr,
-                        end_row,
-                        end_col,
-                        end_row,
-                        end_col,
-                        { string.format(" className=%s", new_classes) }
-                    )
-                end
-            end
+            myfunc(old_winnr, old_bufnr, item)
         end,
     })
 
