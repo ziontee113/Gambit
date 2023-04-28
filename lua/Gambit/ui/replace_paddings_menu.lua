c421289d8b2425bed8a3fefe7012fb8377acdac9
ziontee113 <ziontee113@gmail.com>
2023-04-28 11:59:47 +0700

    feat(rep-pad): allow hiding menu entries

--- lua/Gambit/ui/replace_paddings_menu.lua
+++ lua/Gambit/ui/replace_paddings_menu.lua
@@ -1,31 +1,48 @@
 local M = {}
 
 local key_value_map = {
-    { "n", "0" },
+    { "0", "", hide = true },
+    { "1", "1", hide = true },
+    { "2", "2", hide = true },
+    { "3", "3", hide = true },
+    { "4", "4", hide = true },
+    { "5", "5", hide = true },
+    { "6", "6", hide = true },
+    { "7", "7", hide = true },
+    { "8", "8", hide = true },
+    { "9", "9", hide = true },
+    -- "",
     { "w", "10" },
     { "e", "11" },
     { "r", "12" },
     { "t", "14" },
-
+    "",
     { "y", "16" },
     { "u", "20" },
     { "i", "24" },
     { "o", "28" },
     { "p", "32" },
-
+    "",
     { "a", "36" },
     { "s", "40" },
     { "d", "44" },
     { "f", "48" },
     { "g", "52" },
-
+    "",
     { "z", "56" },
     { "x", "60" },
     { "c", "64" },
     { "v", "72" },
     { "b", "80" },
+    { "n", "96" },
 
-    { "m", "96" },
+    "",
+    { "m", "0" },
+
+    { ")", "0.5", hide = true },
+    { "!", "1.5", hide = true },
+    { "@", "2.5", hide = true },
+    { "#", "3.5", hide = true },
 }
 
 local Menu = require("nui.menu")
@@ -33,15 +50,27 @@ local Menu = require("nui.menu")
 local defaults = require("Gambit.options.defaults")
 local class_replacer = require("Gambit.lib.class-replacer")
 
+local get_class = function(value)
+    local class = ""
+    if value ~= "" then
+        class = "p-" .. value
+    end
+    return class
+end
+
 M.show_menu = function()
     local old_winnr, old_bufnr = vim.api.nvim_get_current_win(), vim.api.nvim_get_current_buf()
 
     local lines = {}
-    for i, _ in ipairs(key_value_map) do
-        local key, value = unpack(key_value_map[i])
-        local class = "p-" .. value
-        local display_text = string.format("%s %s", key, class)
-        table.insert(lines, Menu.item(display_text, { data = class }))
+    for _, entry in ipairs(key_value_map) do
+        if type(entry) == "string" then
+            table.insert(lines, Menu.separator(entry))
+        elseif type(entry) == "table" and not entry.hide then
+            local key, value = unpack(entry)
+            local class = get_class(value)
+            local display_text = string.format("%s %s", key, class)
+            table.insert(lines, Menu.item(display_text, { data = class }))
+        end
     end
 
     local menu = Menu(defaults.popup_options, {
@@ -60,45 +89,18 @@ M.show_menu = function()
 
     --------------------------------------------
 
-    for i, _ in pairs(key_value_map) do
-        local key, value = unpack(key_value_map[i])
-        menu:map("n", key, function()
-            menu:unmount()
-            local class = "p-" .. value
-            class_replacer.change_tailwind_paddings(old_winnr, old_bufnr, class)
-        end, { nowait = true })
+    for _, entry in pairs(key_value_map) do
+        if type(entry) == "table" then
+            local key, value = unpack(entry)
+            menu:map("n", key, function()
+                menu:unmount()
+                local class = get_class(value)
+                class_replacer.change_tailwind_paddings(old_winnr, old_bufnr, class)
+            end, { nowait = true })
+        end
     end
 
     --------------------------------------------
-
-    local quick_number_map = {
-        ["0"] = "",
-        ["1"] = "1",
-        ["2"] = "2",
-        ["3"] = "3",
-        ["4"] = "4",
-        ["5"] = "5",
-        ["6"] = "6",
-        ["7"] = "7",
-        ["8"] = "8",
-        ["9"] = "9",
-
-        [")"] = "0.5",
-        ["!"] = "1.5",
-        ["@"] = "2.5",
-        ["#"] = "3.5",
-    }
-
-    for key, value in pairs(quick_number_map) do
-        menu:map("n", key, function()
-            menu:unmount()
-            local text = ""
-            if value ~= "" then
-                text = "p-" .. value
-            end
-            class_replacer.change_tailwind_paddings(old_winnr, old_bufnr, text)
-        end)
-    end
 
     menu:mount()
 end
