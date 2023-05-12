ef77c7638b3fb5df7e89de3997dc9a58901a172a
ziontee113 <ziontee113@gmail.com>
2023-05-12 20:14:44 +0700

    feat(uf): create PopUp metatable

--- lua/Gambit/lib/ui/window.lua
+++ lua/Gambit/lib/ui/window.lua
@@ -28,65 +28,70 @@ end
 
 local set_hide_keymaps = function(win, buf, keymaps)
     for _, mapping in ipairs(keymaps) do
-        vim.keymap.set("n", mapping, function() vim.api.nvim_win_hide(win) end, { buffer = buf })
+        vim.keymap.set(
+            "n",
+            mapping,
+            function() vim.api.nvim_win_hide(win) end,
+            { buffer = buf, nowait = true }
+        )
     end
 end
 
 -------------------------------------------- Win Related
 
-local open_win = function(buf, opts)
-    local win = vim.api.nvim_open_win(buf, true, {
+local PopUp = {}
+PopUp.__index = PopUp
+
+function PopUp:new(opts)
+    local popup = setmetatable(opts, PopUp)
+
+    popup.buf = vim.api.nvim_create_buf(false, true)
+
+    local lines = get_lines_from_items(opts.items)
+    vim.api.nvim_buf_set_lines(popup.buf, 0, -1, false, lines)
+
+    popup.width = find_longest_line(lines)
+    popup.height = #opts.items
+
+    return popup
+end
+
+function PopUp:show()
+    self.win = vim.api.nvim_open_win(self.buf, true, {
         relative = "cursor",
         row = 1,
         col = 1,
-        width = opts.width,
-        height = opts.height,
+        width = self.width,
+        height = self.height,
         style = "minimal",
         border = "single",
-        title = opts.title or "",
-        title_pos = opts.title_pos or "center",
+        title = self.title or "",
+        title_pos = self.title_pos or "center",
     })
 
-    vim.api.nvim_win_set_option(win, "winhl", opts.winhl or "Normal:Normal,FloatBorder:@function")
-    vim.api.nvim_win_set_option(win, "cursorline", true)
-
-    return win
+    vim.api.nvim_win_set_option(
+        self.win,
+        "winhl",
+        self.winhl or "Normal:Normal,FloatBorder:@function"
+    )
+    vim.api.nvim_win_set_option(self.win, "cursorline", true)
+    set_hide_keymaps(self.win, self.buf, self.keymaps and self.keymaps.hide or { "q", "<Esc>" })
 end
 
 --------------------------------------------
 
-M.new = function(opts)
-    local buf = vim.api.nvim_create_buf(false, true)
+local popup = PopUp:new({
+    title = "Test",
+    items = {
+        { keymaps = { "h" }, text = "hello" },
+        { keymaps = { "v" }, text = "venus" },
+    },
+    keymaps = {
+        hide = { "z", "q", "<Esc>" },
+    },
+})
 
-    local lines = get_lines_from_items(opts.items)
-    vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
-
-    opts.width = find_longest_line(lines)
-    opts.height = #opts.items
-
-    local win = open_win(buf, opts)
-
-    set_hide_keymaps(win, buf, opts.keymap and opts.keymap.hide or { "q", "<Esc>" })
-
-    return win, buf
-end
-
---------------------------------------------
-
-vim.keymap.set(
-    "n",
-    "<leader>a",
-    function()
-        M.new({
-            title = "Test",
-            items = {
-                { keymaps = { "h" }, text = "hello" },
-                { keymaps = { "v" }, text = "venus" },
-            },
-        })
-    end,
-    {}
-)
+vim.keymap.set("n", "<leader>a", function() popup:show() end, {})
 
 return M
 
